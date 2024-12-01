a5603d7566b58f492eb66944e0fa4c76411f56e7
ashashlin <104874547+ashashlin@users.noreply.github.com>
2024-12-02 00:49:56 +0800

    finish all

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const openNavBtn = document.querySelector('.js-header__nav-toggle');
 const nav = document.querySelector('.header__nav');
 const closeNavBtn = document.querySelector('.js-header__nav-close-btn');
+const navDropdownBtns = document.querySelectorAll('.js-header__nav-dropdown-btn');
+const navDropdownLists = document.querySelectorAll('.js-header__nav-dropdown-list');
 
 openNavBtn.addEventListener('click', () => {
   nav.classList.add('active');
@@ -8,4 +10,24 @@ openNavBtn.addEventListener('click', () => {
 
 closeNavBtn.addEventListener('click', () => {
   nav.classList.remove('active');
+
+  navDropdownLists.forEach((list) => {
+    if (list.classList.contains('active')) {
+      list.classList.toggle('active');
+    }
+  });
+
+  navDropdownBtns.forEach((button) => {
+    if (button.classList.contains('rotate')) {
+      button.classList.toggle('rotate');
+    }
+  });
+});
+
+navDropdownBtns.forEach((button) => {
+  button.addEventListener('click', () => {
+    button.nextElementSibling.classList.toggle('active');
+
+    button.classList.toggle('rotate');
+  });
 });
